e35f71ed1ee5af054ded81d6ef8ba959a607255e
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-07-07 15:46:12 -0400

    add environment database url

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
+const url = process.env.DATABASE_URL || "postgres://localhost:5432/reader";
 
-const db = new Sequelize("postgres://localhost:5432/reader", {
+const db = new Sequelize(url, {
   logging: false, // set to console.log to see the raw SQL queries
   native: true // lets Sequelize know we can use pg-native for ~30% more speed
 });
