563bfd509881568ad363474b9513687abc5008a2
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-07-07 12:02:02 -0400

    add chapter route

--- app.js
+++ app.js
@@ -1,67 +1,20 @@
 const express = require("express");
 const { resolve } = require("path");
-const request = require("request");
-const { JSDOM } = require("jsdom");
+
+const chapterRouter = require("./chapter.js");
 
 const app = express();
 
 app.use(express.static(resolve(__dirname, "public")));
 
-let latestChapter = 1;
-let options;
-requestOptions();
-
-app.get("/chapter/where", (req, res, next) => {
-  res.send(String(latestChapter));
-});
-
-app.get("/chapter/options", (req, res, next) => {
-  requestOptions(res);
-});
-
-app.get("/chapter/:chapter", (req, res, next) => {
-  const chapter = parseInt(req.params.chapter, 10);
-  latestChapter = chapter;
-
-  if (!options) {
-    requestOptions(res);
-  } else {
-    findChapter(res, chapter);
-  }
-});
-
-function requestOptions(res) {
-  const toc = "https://wwyxhqc.wordpress.com/%E4%BF%AE%E7%9C%9F%E4%B8%96%E7%95%8C-world-of-cultivation/table-of-contents/";
-  request(toc, (tocErr, tocRes, tocBody) => {
-    if (tocErr) {
-      if (res) res.send(tocErr);
-      else console.log("ToC error", tocErr);
-    } else {
-      options = new JSDOM(tocBody).window.document.getElementsByTagName("option");
-    }
-  });
-}
-
-function findChapter(res, chapter) {
-  for (var i = 0; i < options.length; i++) {
-    if (options[i].value.indexOf(`${chapter}`) !== -1 && options[i].value.indexOf("world-of-cultivation") !== -1) {
-      // requests chapter url if chapter is found
-      request(options[i].value, (err, response, body) => {
-        if (err) {
-          res.send(err);
-        } else {
-          const text = new JSDOM(body).window.document.getElementsByClassName("entry-content")[0].innerHTML;
-          res.send(text);
-        }
-      });
-      break;
-    }
-  }
-}
+app.use("/chapter", chapterRouter);
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-  console.log("Listening on port", server.address().port);
-});
+
+if (module === require.main) {
+  const server = app.listen(port, () => {
+    console.log("Listening on port", server.address().port);
+  });
+}
 
 module.exports = app;
---
+++ chapter.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const request = require("request");
+const { JSDOM } = require("jsdom");
+
+const router = express.Router();
+
+let latestChapter = 1;
+let options;
+requestOptions();
+
+router.get("/where", (req, res, next) => {
+  res.send(String(latestChapter));
+});
+
+router.get("/options", (req, res, next) => {
+  requestOptions(res);
+});
+
+router.get("/:chapter", (req, res, next) => {
+  const chapter = parseInt(req.params.chapter, 10);
+  latestChapter = chapter;
+
+  if (!options) {
+    requestOptions(res);
+  } else {
+    findChapter(res, chapter);
+  }
+});
+
+function requestOptions(res) {
+  const toc = "https://wwyxhqc.wordpress.com/%E4%BF%AE%E7%9C%9F%E4%B8%96%E7%95%8C-world-of-cultivation/table-of-contents/";
+  request(toc, (tocErr, tocRes, tocBody) => {
+    if (tocErr) {
+      if (res) res.send(tocErr);
+      else console.log("ToC error", tocErr);
+    } else {
+      options = new JSDOM(tocBody).window.document.getElementsByTagName("option");
+      if (res) res.sendStatus(200);
+    }
+  });
+}
+
+function findChapter(res, chapter) {
+  for (var i = 0; i < options.length; i++) {
+    if (options[i].value.indexOf(`${chapter}`) !== -1 && options[i].value.indexOf("world-of-cultivation") !== -1) {
+      // requests chapter url if chapter is found
+      request(options[i].value, (err, response, body) => {
+        if (err) {
+          res.send(err);
+        } else {
+          const text = new JSDOM(body).window.document.getElementsByClassName("entry-content")[0].innerHTML;
+          res.send(text);
+        }
+      });
+      break;
+    }
+  }
+}
+
+module.exports = router;
